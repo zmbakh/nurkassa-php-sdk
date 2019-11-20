3c3d74a28bf44e6d5287b9787340f7a5197baf26
Zangar <zmbakh@gmail.com>
2019-11-20 16:38:15 +0600

    1.2.0

--- src/Nurkassa.php
+++ src/Nurkassa.php
@@ -18,7 +18,7 @@ class Nurkassa
     /**
      * @const string Current SDK version
      */
-    const CURRENT_SDK_VERSION = '1.1.0';
+    const CURRENT_SDK_VERSION = '1.2.0';
 
     /**
      * @var string Access token
